bc5d7c74ee3965231eae31f64fcd6dce6bc3a551
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-02 23:14:28 +0200

    update models

---
+++ models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, Date, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    phone_number = Column(String)
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    room_number = Column(String, unique=True)
+    floor = Column(Integer)
+    number_of_bedrooms = Column(Integer)
+    bed_type = Column(String)
+    occupancy_limit = Column(Integer)
+    ammenities = Column(list[str])
+    status = Column(String)
+    price = Column(Float)
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    reference = Column(String, unique=True)
+    booking_date = Column(Date)
+    check_in_date = Column(Date)
+    check_out_date = Column(Date)
+    price = Column(Float)
+
+    customer_id = Column(Integer, ForeignKey("users.id"))
+    room_id = Column(Integer, ForeignKey("rooms.id"))
+
+
+# reviews 
+

2f51e776eb4b83bdde24a8e1827ae11b7bf18b4c
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-05 21:15:49 +0200

    add api endpoints

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
+from utils import query_filter
 import models, schemas, crud
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,14 +18,42 @@ def get_db():
         db.close()
     
 
-@app.post('/users/', response_model=schemas.User)
+@app.post('/api/users/', response_model=schemas.User)
 def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db=db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered!")
     return crud.create_user(db=db, user=user)
 
-@app.get('/users/', response_model=list[schemas.User])
+@app.get('api/users/', response_model=list[schemas.User])
 def read_users(db: Session = Depends(get_db)):
     users = crud.get_users(db=db)
     return users
+
+
+@app.post('/api/hotel/rooms/', response_model=schemas.Room)
+def add_room(room: schemas.RoomBase, db: Session = Depends(get_db)):
+    db_room = crud.get_room_by_room_number(db=db, room_number=room.room_number)
+    if db_room:
+        raise HTTPException(status_code=400, detail=f"Room {room.room_number} already added!")
+    return crud.add_room(db=db, room=room)
+
+@app.get('/api/hotel/rooms/', response_model=list[schemas.Room])
+def get_rooms(db: Session = Depends(get_db), room_number: str | None = None, floor: int | None = None, number_of_bedrooms: int | None = None, occupancy_limit: int | None = None, status: models.Status | None = None):
+    query = {
+        "room_number": room_number,
+        "floor": floor,
+        "number_of_bedrooms": number_of_bedrooms, 
+        "occupancy_limit": occupancy_limit,
+        "status": status.lower()
+    }
+
+    filter = query_filter(query)
+
+    rooms = crud.get_rooms_by_filter(db=db, filter=filter)
+    return rooms
+
+
+@app.post('/api/hotel/bookings/', response_model=schemas.Booking)
+def create_booking(booking: schemas.BookingBase, db: Session = Depends(get_db)):
+    return crud.create_booking(db, booking=booking)

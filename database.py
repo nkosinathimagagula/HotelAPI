9f9c1517cbb7b81331b5159f460e7bc985307cc7
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-09 20:41:59 +0200

    database updates

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///main_database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///C:\\Users\\nkosinathi\\Documents\\vscode\\APIs\\HotelApi\\sqlalchemy_database\\main_database.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 

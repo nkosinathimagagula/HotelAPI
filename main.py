6654e0ebd4fff02304029f6fd0246a4490469e30
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-09 13:00:39 +0200

    add endpoint for generating the token for each user and secure the other endpoints

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from typing import Annotated
+from datetime import timedelta
 from database import SessionLocal, engine
 from utils import query_filter
-import models, schemas, crud
+import models, schemas, crud, security
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -18,6 +21,28 @@ def get_db():
         db.close()
     
 
+# login
+@app.post('/token', response_model=schemas.Token)
+def generate_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = security.authenticate_user(db=db, username=form_data.username, password=form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    
+    access_token_exp = timedelta(minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    access_token = security.create_access_token(
+        details={"sub": user.email},
+        expires=access_token_exp
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 @app.post('/api/users/', response_model=schemas.User)
 def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db=db, email=user.email)
@@ -25,21 +50,35 @@ def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered!")
     return crud.create_user(db=db, user=user)
 
-@app.get('api/users/', response_model=list[schemas.User])
-def read_users(db: Session = Depends(get_db)):
+@app.get('/api/users/', response_model=list[schemas.User])
+def read_users(token: Annotated[str, Depends(security.oath2_scheme)], db: Session = Depends(get_db)):
+    security.validate_admin_token(db=db, token=token)
+    
     users = crud.get_users(db=db)
     return users
 
 
 @app.post('/api/hotel/rooms/', response_model=schemas.Room)
-def add_room(room: schemas.RoomBase, db: Session = Depends(get_db)):
+def add_room(token: Annotated[str, Depends(security.oath2_scheme)], room: schemas.RoomBase, db: Session = Depends(get_db)):
+    security.validate_admin_token(db=db, token=token)
+    
     db_room = crud.get_room_by_room_number(db=db, room_number=room.room_number)
     if db_room:
         raise HTTPException(status_code=400, detail=f"Room {room.room_number} already added!")
     return crud.add_room(db=db, room=room)
 
 @app.get('/api/hotel/rooms/', response_model=list[schemas.Room])
-def get_rooms(db: Session = Depends(get_db), room_number: str | None = None, floor: int | None = None, number_of_bedrooms: int | None = None, occupancy_limit: int | None = None, status: models.Status | None = None):
+def get_rooms(
+    token: Annotated[str, Depends(security.oath2_scheme)],
+    db: Session = Depends(get_db), 
+    room_number: str | None = None, 
+    floor: int | None = None, 
+    number_of_bedrooms: int | None = None, 
+    occupancy_limit: int | None = None, 
+    status: schemas.Status | None = None
+):
+    security.validate_token(db=db, token=token)
+    
     query = {
         "room_number": room_number,
         "floor": floor,
@@ -54,20 +93,24 @@ def get_rooms(db: Session = Depends(get_db), room_number: str | None = None, flo
     return rooms
 
 @app.put('/api/hotel/rooms/{room_number}', response_model=schemas.Room)
-def update_room_price(room_number: str, price: str, db: Session = Depends(get_db)):
+def update_room_price(room_number: str, price: str, token: Annotated[str, Depends(security.oath2_scheme)], db: Session = Depends(get_db)):
+    security.validate_admin_token(db=db, token=token)
+    
     room = crud.get_room_by_room_number(db=db, room_number=room_number)
     return crud.update_room_info(db=db, room_id=room.id, value_to_update={"price": price})
 
-@app.put('/api.hotel/rooms/check-out/{reference}', response_model=schemas.Room)
-def update_room(reference: str, db:Session = Depends(get_db)):
+@app.put('/api/hotel/rooms/check-out/{reference}', response_model=schemas.Room)
+def update_room(token: Annotated[str, Depends(security.oath2_scheme)], reference: str, db:Session = Depends(get_db)):
+    security.validate_token(db=db, token=token)
+    
     booking = crud.get_booking(db=db, reference=reference)
     return crud.update_room_info(db=db, room_id=booking.room_id, value_to_update={"status": "available"})
 
 
 
 @app.post('/api/hotel/bookings/', response_model=schemas.Booking)
-def create_booking(booking: schemas.BookingBase, db: Session = Depends(get_db)):
+def create_booking(token: Annotated[str, Depends(security.oath2_scheme)], booking: schemas.BookingBase, db: Session = Depends(get_db)):
+    security.validate_token(db=db, token=token)
+        
     return crud.create_booking(db, booking=booking)
-
-
-
+    
